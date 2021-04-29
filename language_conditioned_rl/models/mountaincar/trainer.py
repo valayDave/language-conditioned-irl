9d34ef67610289334bd47500cf2e39ad1645fb5c
Valay Dave <valaygaurang@gmail.com>
2021-04-29 11:28:31 -0700

    RL Trainer fixes.

--- language_conditioned_rl/models/mountaincar/trainer.py
+++ language_conditioned_rl/models/mountaincar/trainer.py
@@ -24,7 +24,6 @@ def create_logger(logger_name:str,level=logging.INFO):
 
 class Trainer:
     def __init__(self,\
-                text_context=CHOSEN_TEXT, \
                 model_hidden=256,\
                 num_eps=50000,\
                 num_timesteps=200,\
@@ -41,7 +40,6 @@ class Trainer:
         
         if api_token is None:
             print("No API Key Added : Will Start Offline Session")
-        self.text_context = text_context
         self.model_hidden = model_hidden
         self.num_eps = num_eps
         self.num_timesteps = num_timesteps
@@ -57,19 +55,25 @@ class Trainer:
         self.log_every=log_every
 
 
-    def get_core_config(self,reward_fn:LGRMountainCarInferenceMixin):
+    def get_core_config(self,reward_fn:LGRMountainCarInferenceMixin,text_context:str=None):
         ENV = "MountainCar-v0"
+        reward_ob = dict(used_rw_fn = False)
+        if reward_fn is not None:
+            reward_ob = dict(
+                **reward_ob,
+                run_name=reward_fn.experiment_name,
+                model_pth=reward_fn.loaded_checkpoint,
+            )
         config = dict(
             reward_scaleup=self.reward_scaleup,
             reward_min=self.reward_min,
             reward_max=self.reward_max,
             env=ENV,
-            text_context=self.text_context,
+            text_context=text_context,
             model_hidden=self.model_hidden,
             num_eps=self.num_eps,
             num_timesteps=self.num_timesteps,
-            run_name=reward_fn.experiment_name,
-            model_pth=reward_fn.loaded_checkpoint,
+            **reward_ob
         )
         return config
 
@@ -92,9 +96,9 @@ class Trainer:
         return traj_tuples ,states ,actions
 
 
-    def run(self,reward_fn:LGRMountainCarInferenceMixin,render:bool=False):
+    def run(self,reward_fn:LGRMountainCarInferenceMixin,render:bool=False,seed:int=None,text_context:str=CHOSEN_TEXT):
         import neptune
-        config = self.get_core_config(reward_fn)
+        config = self.get_core_config(reward_fn,text_context=text_context)
         # $ Create Logger. 
         if self.api_token is not None:
             neptune.init(self.project_name,api_token=self.api_token)
@@ -107,12 +111,15 @@ class Trainer:
         # $ Create new Agent And Start the Episode.
         agent = SARSALambdaAgent(env)
         logger = create_logger(self.experiment_name)
+        if seed is not None:
+            env.seed(seed=seed)
+
         try:
             for e in range(self.num_eps):
                 # $ run the episode 
                 traj_tuples ,states ,actions = self.run_episode(env,agent,render=render)
                 # $ Get the rewards from Trajectory. 
-                reward = reward_fn.get_rewards(states, actions, self.text_context)
+                reward = reward_fn.get_rewards(states, actions, text_context)
                 # $ Normalize and Ammortize the rws
                 reward = RewardNormalizer.normalize_reward(reward, \
                                             max_v=self.reward_max,\
@@ -136,6 +143,64 @@ class Trainer:
                 neptune.log_artifact(self.video_save_dir)
             neptune.stop()
             return 
+        env.close()
+        if self.video_save_dir:
+            neptune.log_artifact(self.video_save_dir)
+        neptune.stop()
+
+    
+    def play_sarsa_native(self,env:gym.Env,agent:SARSALambdaAgent,render:bool=False,train:bool=True):
+        episode_reward = 0
+        observation = env.reset()
+        action = agent.act(observation)
+        while True:
+            if render:
+                env.render()
+            observation_next, reward, done, _ = env.step(action)
+            episode_reward += reward
+            if done:
+                if train:
+                    agent.learn(observation, action, reward, observation_next, done)
+                break
+            action_next = agent.act(observation_next)
+            if train:
+                agent.learn(observation, action, reward, observation_next, done, action_next)
+            observation, action = observation_next, action_next
+        return episode_reward
+        
+
+    def run_native(self,train=True,render=False,seed:int=None):
+        import neptune
+        config = self.get_core_config(None,)
+        # $ Create Logger. 
+        if self.api_token is not None:
+            neptune.init(self.project_name,api_token=self.api_token)
+        else:
+            neptune.init(self.project_name,backend=neptune.OfflineBackend())
+
+        neptune.create_experiment(name=self.experiment_name, params=config)
+        # $ Gets Mountain Car environment. 
+        env = get_env(video_save_dir=self.video_save_dir,episode_save_freq=self.video_save_freq)
+        # $ Create new Agent And Start the Episode.
+        agent = SARSALambdaAgent(env)
+        logger = create_logger(self.experiment_name)
+        if seed is not None:
+            env.seed(seed=seed)
+        try:
+            for e in range(self.num_eps):
+                episodereward = self.play_sarsa_native(env,agent,render=render,train=train)
+                neptune.log_metric('reward', e, y=episodereward)
+                if e % self.log_every == 0:
+                    logger.info(f'Completed Episode {e} With Reward {episodereward}')
+
+        except KeyboardInterrupt as e:
+            logger.error("Keyboard Interrupt Occured")
+            env.close()
+            if self.video_save_dir:
+                neptune.log_artifact(self.video_save_dir)
+            neptune.stop()
+            return 
+        
         env.close()
         if self.video_save_dir:
             neptune.log_artifact(self.video_save_dir)
